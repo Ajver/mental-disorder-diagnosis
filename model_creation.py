8977c59fae033e03afc9f7c8490e7b6f93d0d8b7
Dominik <zawlockidominik@gmail.com>
2025-12-28 14:29:15 +0100

    Create helper model_creation module

---
+++ model_creation.py
@@ -0,0 +1,38 @@
+import json
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFE
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+
+def create_random_forest(params: dict) -> Pipeline:
+    n_features_to_select = params["n_features_to_select"]
+    n_estimators = params["n_estimators"]
+    min_samples_split = params["min_samples_split"]
+
+    scaler = {
+        "StandardScaler": StandardScaler(),
+        "MinMaxScaler": MinMaxScaler(),
+    }[params["scaler_name"]]
+
+    model = RandomForestClassifier(n_estimators=n_estimators, min_samples_split=min_samples_split)
+    pipe = Pipeline([
+        ("scaler", scaler),
+        ("selector", RFE(model, n_features_to_select=n_features_to_select)),
+        ("classifier", model),
+    ])
+    return pipe
+
+
+def save_model_params(model_name: str, params: dict):
+    with open(f"model_params/{model_name}.json", "w") as f:
+        params_str = json.dumps(params, indent=4)
+        f.write(params_str)
+
+
+def load_model_params(model_name: str) -> dict:
+    with open(f"model_params/{model_name}.json", "r") as f:
+        params_str = f.read()
+        params = json.loads(params_str)
+        return params
